6d2060875643178540c8b339ac5945ae7b0947b0
mattmass1 <massmatt371@gmail.com>
2026-05-31 17:36:52 -0400

    Add email/purchase updates and a forEach display

--- cc_3.js
+++ cc_3.js
@@ -8,3 +8,12 @@ const customers=[
 customers.push({name:"Matthew",email:"Matthew@123.com", purchases:["tablet"]});
 customers.shift();
 console.log(customers);
+
+
+customers[0].email="Bob@1234.com";
+customers[0].purchases.push("gift-cards");
+
+
+customers.forEach((customer) => {
+  console.log(`${customer.name} | ${customer.email} | ${customer.purchases.length} purchases`);
+});
